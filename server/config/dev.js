d3d732fadcd592768b81236e92b94a8bfd4e3f5b
Christina Kurpiel <kurpielcm@gmail.com>
2020-11-09 14:29:00 -0500

    npm install dependencies trying to fish deployment issues

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGODB_URI: 'mongodb+srv://christinak:Ey6RqVp2i8G2W5p@calculatordb.pgf5z.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    MONGODB_URI: 'mongodb://localhost/calulator',
     TOKEN_SECRET: 'Ey6RqVp2i8G2W5p'
   };
   
